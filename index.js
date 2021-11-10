785d636bd13b73c244b8c1e69622141170aa04af
m <m@ms-MacBook-Air.local>
2021-11-10 13:31:54 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
