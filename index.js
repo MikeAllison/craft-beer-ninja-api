ac2fbd21e6599bee865c76df80978eb9213eaea5
mikeallison <mikeallison79@gmail.com>
2021-11-13 11:24:07 -0500

    Testing

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/api/search', (req, res) => {
+  const places = [];
+
+  places.push(
+    { id: '1', name: 'Solid State' },
+    { id: '2', name: 'Sweet Avenue' },
+    { id: '3', name: 'Astoria Bier & Cheese' }
+  );
+
+  res.status(200).json(places);
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
