2cd18ec633ab74b81c8b66f0106ad8f56609e51d
m <mikeallison79@gmail.com>
2021-11-30 19:22:00 -0500

    Started working on Places API requests

--- index.js
+++ index.js
@@ -6,32 +6,91 @@ const app = express();
 app.use(express.json());
 
 app.get('/api/v1/places', (req, res) => {
-  const coords = `40.7404654,-73.9174162`;
+  const coords = `40.740627371382296, -73.91768744610071`;
   const keyword = 'craft_beer';
-  const radius = 50000;
   const rankby = 'distance';
 
-  var params = {
-    method: 'get',
-    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords}&keyword=${keyword}&rankby=${rankby}&key=${process.env.GOOGLE_API_KEY}`
+  let results = [];
+
+  // const requestPlaces = params => {
+  //   axios(params)
+  //     .then(response => {
+  //       // TO-DO: Refactor
+  //       if (response.data.status === 'ZERO_RESULTS') {
+  //         res.status(200).json({});
+  //       } else if (response.data.status === 'OK') {
+  //         console.log(response.data);
+  //         results.push(...response.data.results);
+  //         return results;
+  //       } else {
+  //         if (response.data.error_message) {
+  //           console.log(response.data);
+  //           res.status(500).json(response.data.error_message);
+  //         } else {
+  //           console.log(response.data);
+  //           res.status(500).json(response.data);
+  //         }
+  //       }
+  //     })
+  //     .then(data => {
+  //       res.status(200).json(data);
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //       res.status(500);
+  //     });
+  // };
+
+  const requestNextPage = nextPageToken => {
+    axios
+      .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${nextPageToken}&key=${process.env.GOOGLE_API_KEY}`)
+      .then(response => {
+        console.log('IN REQ NEXT PAGE');
+        //console.log(response);
+        results.push(...response.data.results);
+        //console.log(results);
+        console.log('Results:' + results.length);
+        console.log(response.data.next_page_token);
+        if (response.data.next_page_token) {
+          return response.data.next_page_token;
+        } else {
+          return null;
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
-  axios(params)
-    .then(response => {
-      // TO-DO: Refactor
-      if ((response.status = 'ZERO_RESULTS')) {
-        throw new Error('The search returned 0 results');
-      } else if ((response.results = 'OK')) {
-        const results = response.data;
-        res.status(200).json(results);
-      } else {
-        throw new Error(results.error_message);
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500);
-    });
+  const requestPlaces = () => {
+    axios
+      .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords}&keyword=${keyword}&rankby=${rankby}&key=${process.env.GOOGLE_API_KEY}`)
+      .then(response => {
+        results.push(...response.data.results);
+        //console.log(results);
+        console.log('Results:' + results.length);
+        let next_page_token = response.data.next_page_token;
+        console.log(next_page_token);
+        if (next_page_token) {
+          setTimeout(() => {
+            console.log('CALLING REQ NEXT PAGE');
+            next_page_token = requestNextPage(response.data.next_page_token);
+            console.log('AFTER REQ NEXT PAGE');
+            console.log(next_page_token);
+          }, 5000);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  // Make initial request
+  // If response.data.next_page_token
+  // Make another request
+  // If
+
+  requestPlaces();
 });
 
 app.listen(process.env.PORT, () => {
