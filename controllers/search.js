95b0c4277371fa74e30015630a8361a075228ea3
mikeallison <mikeallison79@gmail.com>
2021-12-02 14:31:24 -0500

    Added console.logs for debugging

--- controllers/search.js
+++ controllers/search.js
@@ -9,24 +9,28 @@ exports.requestPlaces = (req, res, next) => {
   //`https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${nextPageToken}&key=${process.env.GOOGLE_API_KEY}`
 
   axios
-    .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords}&keyword=${keyword}&rankby=${rankby}&key=${process.env.GOOGLE_API_KEY}`)
-    .then(response => {
+    .get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords}&keyword=${keyword}&rankby=${rankby}&key=${process.env.GOOGLE_API_KEY}`
+    )
+    .then((response) => {
       if (response.data.status === 'ZERO_RESULTS') {
         res.status(200).json({ results: [], next_page_token: null });
       } else if (response.data.status === 'OK') {
         res.status(200).json({
           results: response.data.results,
-          next_page_token: response.data.next_page_token
+          next_page_token: response.data.next_page_token,
         });
       } else {
         if (response.data.error_message) {
+          console.log(response.data.error_message);
           res.status(500).json(response.data.error_message);
         } else {
+          console.log(response.data);
           res.status(500).json(response.data);
         }
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
       res.status(500);
     });
