4ecd0d224b4c174030f53a49824d4bc7a8d701c7
m <mikeallison79@gmail.com>
2022-01-16 11:40:59 -0500

    Added HTTPS redirect code

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const searchRoutes = require('./routes/search');
 
 const app = express();
 
+// HTTPS redirect
+if (process.env.NODE_ENV === 'production') {
+  app.enable('trust proxy');
+  app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url);
+  });
+}
+
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({ origin: `${process.env.CORS_ORIGIN}` }));
 
 app.use('/api/v1', searchRoutes);
 
